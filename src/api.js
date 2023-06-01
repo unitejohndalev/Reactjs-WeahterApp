573606bd259fffa1551b54ce4162df8f841f1fe4
John Dale <unitejohndalev@gmail.com>
2023-06-01 19:27:57 +0800

    created using reactjs

---
+++ src/api.js
@@ -0,0 +1,11 @@
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c6d965905fmsh0e3f67a6bbf6164p1fb582jsn640a0bdc4905",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
