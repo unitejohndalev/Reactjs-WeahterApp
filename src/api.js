4158246f20b85de985d58a7962105aab2cda3f03
John Dale <unitejohndalev@gmail.com>
2023-06-02 09:56:10 +0800

    last update for today

--- src/api.js
+++ src/api.js
@@ -9,3 +9,7 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL =
+  "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "8fb9f0fe8093cb1df154b714f4460013";
